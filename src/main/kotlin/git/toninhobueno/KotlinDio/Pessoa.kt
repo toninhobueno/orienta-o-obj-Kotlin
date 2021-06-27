1ec3af07a3d451407ac98355bccc6f38b52e7db0
tonin <antonio.leite00@hotmail.com>
2021-06-27 16:08:48 -0300

    criado objeto pessoa

---
+++ src/main/kotlin/git/toninhobueno/KotlinDio/Pessoa.kt
@@ -0,0 +1,17 @@
+package git.toninhobueno.KotlinDio
+
+import java.lang.NullPointerException
+
+class Pessoa {
+    var nome:String = "Antonio"
+    var cpf:String = "123.456.789-10"
+
+}
+
+fun main (){
+    val antonio = Pessoa()
+
+    println(antonio.nome)
+    println(antonio.cpf)
+
+}
