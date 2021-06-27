00d73cc024283031ca64507af4ae60df5789110a
tonin <antonio.leite00@hotmail.com>
2021-06-27 19:23:56 -0300

    adicionado private set ao cpf

--- src/main/kotlin/git/toninhobueno/KotlinDio/Pessoa.kt
+++ src/main/kotlin/git/toninhobueno/KotlinDio/Pessoa.kt
@@ -4,8 +4,9 @@ import java.lang.NullPointerException
 
 class Pessoa {
     var nome:String = "Antonio"
-    var cpf:String = "123.456.789-10"
 
+    var cpf:String = "123.456.789-10"
+    private set
 }
 
 fun main (){
