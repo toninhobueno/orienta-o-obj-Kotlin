de3d34cb051d50103cebab06af02f12e8e5c1229
tonin <antonio.leite00@hotmail.com>
2021-06-28 19:32:19 -0300

    tornando classes abstratas

--- src/main/kotlin/git/toninhobueno/KotlinDio/Pessoa.kt
+++ src/main/kotlin/git/toninhobueno/KotlinDio/Pessoa.kt
@@ -2,20 +2,7 @@ package git.toninhobueno.KotlinDio
 
 import java.lang.NullPointerException
 
-class Pessoa {
-    var nome:String = "Antonio"
-
-    var cpf:String = "123.456.789-10"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main (){
-    val antonio = Pessoa()
-
-    println(antonio.pessoaInfo())
-
-}
+abstract class Pessoa(
+     val nome: String,
+     val cpf: String
+    )
