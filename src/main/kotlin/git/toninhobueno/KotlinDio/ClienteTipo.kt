cec4daca03785a9ede3b0ffe65fcd32e2894162d
tonin <antonio.leite00@hotmail.com>
2021-06-28 17:23:39 -0300

    Aplicando classes Enum

---
+++ src/main/kotlin/git/toninhobueno/KotlinDio/ClienteTipo.kt
@@ -0,0 +1,8 @@
+package git.toninhobueno.KotlinDio
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica");
+
+
+}
---
+++ src/main/kotlin/git/toninhobueno/KotlinDio/testes/TesteClienteEnum.kt
@@ -0,0 +1,19 @@
+package git.toninhobueno.KotlinDio.testes
+
+import git.toninhobueno.KotlinDio.ClienteTipo
+
+fun main() {
+ClienteTipo.values().forEach { elemento ->
+    println("${elemento.name} - ${elemento.descricao}" )
+}
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao} ")
+    }
+
+    val pf = ClienteTipo.PF
+    println("--${pf.name} - ${pf.descricao} ")
+
+    val pj = ClienteTipo.PJ
+    println("--${pj.name} - ${pj.descricao} ")
+}
